a5596800c3ef8a9c62ce623a32643892ec29752b
Mohammed El Hamdaoui <mel-hamd@e3r10p10.1337.ma>
2023-11-27 12:46:54 +0100

    implementing the get_next_line function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,17 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 12:36:15 by mel-hamd          #+#    #+#             */
-/*   Updated: 2023/11/27 12:36:31 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2023/11/27 12:45:19 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*str;
+	char		*next;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) == -1)
+		return (free(str), str = NULL, NULL);	
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,18 @@
 /*   By: mel-hamd <mel-hamd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 12:36:10 by mel-hamd          #+#    #+#             */
-/*   Updated: 2023/11/27 12:36:34 by mel-hamd         ###   ########.fr       */
+/*   Updated: 2023/11/27 12:45:48 by mel-hamd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+#endif
